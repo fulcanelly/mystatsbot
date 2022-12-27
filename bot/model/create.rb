5a0c8fca8c2e8763ae811c22bc44c4e24b6ddc76
fulcanelly <dallasxfulcanelly@gmail.com>
2022-12-27 07:00:54 +0200

    fut: adjust model creation to tg toolkit

--- bot/model/create.rb
+++ bot/model/create.rb
@@ -1,17 +1,8 @@
 require_relative './connect'
 
 
-class CreateAll < ActiveRecord::Migration[7.0]
+class CreateAll < CoreCreateAll
     def change 
-
-
-        #telegram user 
-        create_table :users, if_not_exists: true do |t|
-            t.string :name
-            t.integer :user_id, :limit => 8
-            
-            t.timestamps 
-        end
 
         create_table :inline_keyboards, if_not_exists: true do |t|
             t.string :dump 
@@ -19,16 +10,6 @@ class CreateAll < ActiveRecord::Migration[7.0]
             t.references :user, null: true, foreign_key: { to_table: :users }
             t.timestamps
         end
-
-        #state of bot
-        create_table :states, if_not_exists: true do |t|
-            t.binary :state_dump 
-
-            t.references :user, null: true, foreign_key: { to_table: :users }
-
-            t.timestamps
-        end
-    
 
         create_table :activities, if_not_exists: true do |t|
             t.string :name 
@@ -47,9 +28,8 @@ class CreateAll < ActiveRecord::Migration[7.0]
             t.timestamps
         end
     
-    
+        super
     end
 end
 
 
-CreateAll.new.change
