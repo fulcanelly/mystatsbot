42f166d8abbeea84dda73f42884e70e4392364d3
fulcanelly <dallasxfulcanelly@gmail.com>
2023-05-18 04:01:03 +0300

    add charts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   #  include ActionView::Layouts
+ # ActionController::API
 
 end
--- app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
@@ -1,12 +1,61 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import {Chart} from "react-google-charts";
 //import style from './HelloWorld.module.css';
+
+
+const ExampleCalendarChart = () => {
+
+    const options = {
+        title: "Red Sox Attendance",
+    }
+
+    const types = [
+        {
+            type: "date",
+            id: "Date",
+        },
+        {
+            type: "number",
+            id: "Won/Loss",
+        },
+    ]
+
+    const data = [
+        types,
+        [new Date(2012, 3, 13), 37032],
+        [new Date(2012, 3, 14), 38024],
+        [new Date(2012, 3, 15), 38024],
+        [new Date(2012, 3, 16), 38108],
+        [new Date(2012, 3, 17), 38229],
+        // Many rows omitted for brevity.
+        [new Date(2013, 9, 4), 38177],
+        [new Date(2013, 9, 5), 38705],
+        [new Date(2013, 9, 12), 38210],
+        [new Date(2013, 9, 13), 38029],
+        [new Date(2013, 9, 19), 38823],
+        [new Date(2013, 9, 23), 38345],
+        [new Date(2013, 9, 24), 38436],
+        [new Date(2013, 9, 30), 38447],
+
+    ]
+
+    return <Chart
+        chartType="Calendar"
+        width="100%"
+        height="400px"
+        data={data}
+        options={options}
+    ></Chart>
+}
+
 
 const HelloWorld = (props) => {
   const [name, setName] = useState(props.name);
 
   return (
     <div>
+      <ExampleCalendarChart/>
       <h3>Hello=, {name}!</h3>
       <hr />
       <form>
