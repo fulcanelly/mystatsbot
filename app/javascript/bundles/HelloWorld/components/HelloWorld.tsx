567dc944c1b602145c91f70c5ba9aff4098df246
fulcanelly <dallasxfulcanelly@gmail.com>
2023-05-18 05:10:54 +0300

    add graph events

--- app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
+++ app/javascript/bundles/HelloWorld/components/HelloWorld.tsx
@@ -1,61 +1,113 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-import {Chart} from "react-google-charts";
+import React, {useEffect, useRef, useState} from 'react';
+import {Chart, ReactGoogleChartEvent} from "react-google-charts";
 //import style from './HelloWorld.module.css';
 
+const options = {
+    title: "Red Sox Attendance",
+}
 
-const ExampleCalendarChart = () => {
-
-    const options = {
-        title: "Red Sox Attendance",
+const types = [
+    {
+        type: "date",
+        id: "Date",
+    },
+    {
+        type: "number",
+        id: "Event amount",
     }
+]
 
-    const types = [
-        {
-            type: "date",
-            id: "Date",
-        },
-        {
-            type: "number",
-            id: "Won/Loss",
-        },
-    ]
+const data = [
+    types,
+    [new Date(2012, 3, 13), 37032],
+    [new Date(2012, 3, 14), 38024],
+    [new Date(2012, 3, 15), 38024],
+    [new Date(2012, 3, 16), 38108],
+    [new Date(2012, 3, 17), 38229],
+    // Many rows omitted for brevity.
+    [new Date(2013, 9, 4), 38177],
+    [new Date(2013, 9, 5), 38705],
+    [new Date(2013, 9, 12), 38210],
+    [new Date(2013, 9, 13), 38029],
+    [new Date(2013, 9, 19), 38823],
+    [new Date(2013, 9, 23), 38345],
+    [new Date(2013, 9, 24), 38436],
+    [new Date(2013, 9, 30), 38447],
+]
 
-    const data = [
-        types,
-        [new Date(2012, 3, 13), 37032],
-        [new Date(2012, 3, 14), 38024],
-        [new Date(2012, 3, 15), 38024],
-        [new Date(2012, 3, 16), 38108],
-        [new Date(2012, 3, 17), 38229],
-        // Many rows omitted for brevity.
-        [new Date(2013, 9, 4), 38177],
-        [new Date(2013, 9, 5), 38705],
-        [new Date(2013, 9, 12), 38210],
-        [new Date(2013, 9, 13), 38029],
-        [new Date(2013, 9, 19), 38823],
-        [new Date(2013, 9, 23), 38345],
-        [new Date(2013, 9, 24), 38436],
-        [new Date(2013, 9, 30), 38447],
 
-    ]
+// const InfoCalendar = () => {
+//
+//     const [selected, setSelected] = useState(null)
+//     const [chartState, setChartState] = useState()
+//
+//     const selectEventHandler = {
+//         eventName: "select",
+//         callback: ({chartWrapper}) => {
+//             const chart = chartWrapper.getChart();
+//             const selection = chart.getSelection();
+//
+//             setSelected(selection)
+//         }
+//     }
+//
+//      return <>
+//           <Chart
+//               state={{}}
+//               chartType="Calendar"
+//               width="100%"
+//               height="400px"
+//               data={data}
+//               options={options}
+//               chartEvents={[selectEventHandler]}
+//           />
+//         {selected && <h2>Currently selected: {JSON.stringify(selected)} </h2>}
+//      </>
+// }
 
+
+
+
+const InfoCalendar = () => {
+
+  const [selected, setSelected] = useState(null)
+
+
+  return <>
+      <ExampleCalendarChart setSelected={setSelected}/>
+      {selected && <h2>Currently selected: {JSON.stringify(selected)} </h2>}
+  </>
+}
+
+
+// Use the state and event handler in the ExampleCalendarChart component
+const ExampleCalendarChart = ({ setSelected }) => {
+
+    const selectEventHandler = {
+        eventName: "select",
+        callback: ({chartWrapper}) => {
+            const chart = chartWrapper.getChart();
+            const selection = chart.getSelection();
+
+            setSelected(selection)
+        }
+    }
     return <Chart
         chartType="Calendar"
         width="100%"
         height="400px"
         data={data}
         options={options}
-    ></Chart>
-}
-
-
+        chartEvents={[selectEventHandler]}
+        />
+};
 const HelloWorld = (props) => {
   const [name, setName] = useState(props.name);
 
   return (
     <div>
-      <ExampleCalendarChart/>
+      <InfoCalendar/>
       <h3>Hello=, {name}!</h3>
       <hr />
       <form>
@@ -72,4 +124,4 @@ HelloWorld.propTypes = {
   name: PropTypes.string.isRequired, // this is passed from the Rails view
 };
 
-export default HelloWorld;
+export default HelloWorld
