5e2ee917c1f409a907e7f121cf569489dd88cf73
fulcanelly <dallasxfulcanelly@gmail.com>
2023-05-20 00:49:57 +0300

    remove site

--- site/app/models/user_stat.rb
+++
@@ -1,2 +0,0 @@
-class UserStat < ApplicationRecord
-end
--- site/db/migrate/20230515210917_create_user_stats.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserStats < ActiveRecord::Migration[7.0]
-  def change
-    create_table :user_stats do |t|
-      t.integer :user_id
-
-      t.timestamps
-    end
-  end
-end
