dafa3f75237087e8a3342a6a4eb3a91e7a22c8b6
fulcanelly <dallasxfulcanelly@gmail.com>
2023-05-21 05:16:36 +0300

    add chats table

--- locasite/app/models/tg_post.rb
+++ locasite/app/models/tg_post.rb
@@ -1,3 +1,9 @@
 class TgPost < ApplicationRecord
+
+  after_find do
+    Chat.find_or_create_by(id: self.chat_id)
+  end
+
   belongs_to :day
+  belongs_to :chat
 end
--- locasite/db/schema.rb
+++ locasite/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_20_203126) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_21_012320) do
+  create_table "chats", force: :cascade do |t|
+    t.string "first_name"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "days", force: :cascade do |t|
     t.date "date"
     t.datetime "created_at", null: false
