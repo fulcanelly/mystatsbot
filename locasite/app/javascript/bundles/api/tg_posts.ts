593d1059a094ce08015afc90435770b1d838b787
fulcanelly <dallasxfulcanelly@gmail.com>
2023-05-23 01:27:18 +0300

    convert to async functions

--- locasite/app/javascript/bundles/api/tg_posts.ts
+++ locasite/app/javascript/bundles/api/tg_posts.ts
@@ -1,20 +1,20 @@
-export function getPostsPerDay(startDate: Date | string, endDate: Date | string) {
+export async function getPostsPerDay(startDate: Date | string, endDate: Date | string) {
     const apiUrl = '/api/v1/tg_posts/posts_per_day';
     const params = {
         start_date: startDate.toString(),
         end_date: endDate.toString()
     };
 
-    return fetch(apiUrl + '?' + new URLSearchParams(params))
-        .then(response => response.json())
+    const response = await fetch(apiUrl + '?' + new URLSearchParams(params));
+    return await response.json();
 }
 
-export function getChatStatsOfDay(day: Date | string) {
+export async function getChatStatsOfDay(day: Date | string) {
     const apiUrl = '/api/v1/tg_posts/chat_stats_of_day';
     const params = {
         day: day.toString()
     };
 
-    return fetch(apiUrl + '?' + new URLSearchParams(params))
-        .then(response => response.json())
+    const response = await fetch(apiUrl + '?' + new URLSearchParams(params));
+    return await response.json();
 }
